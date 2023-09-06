e32e22f080934fc08343ff06282006ba036977cb
GoldLion1011 <sarsh.holbie@Sarahs-MBP-2.lan>
2023-09-06 11:24:33 -0500

    task 0

---
+++ react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './holbielogo.jpg';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} alt="Holberton Logo" />
+                    <h1>School dashboard</h1>
+                </div>
+                <div className="App-body">
+                    <p>Login to access the full dashboard</p>
+                </div>
+                <div className="App-footer">
+                    <p>Copyright 2020 - Holberton School</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
