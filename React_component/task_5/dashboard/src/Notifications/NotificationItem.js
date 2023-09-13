f8b2bd1106583b01a004d520d4a3181a4be4735a
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-09-13 15:21:16 -0500

    rest of 5

--- React_component/task_5/dashboard/src/Notifications/NotificationItem.js
+++ React_component/task_5/dashboard/src/Notifications/NotificationItem.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import './Notifications.css';
 import NotificationItemShape from './NotificationItemShape';
 import PropTypes from 'prop-types';
 
-const NotificationItem = React.memo(({ type, html, value, markAsRead, id }) => {
-  const handleClick = () => {
-    markAsRead(id);
+class NotificationItem extends PureComponent {
+  shouldComponentUpdate(nextProps) {
+    return nextProps.listNotifications.length > this.props.listNotifications.length;
+  }
+
+  handleClick = () => {
+    this.props.markAsRead(this.props.id);
   };
 
-  if (html) {
+  render() {
+    const { type, html, value } = this.props;
+
+    if (html) {
+      return (
+        <li data-notification-type={type} dangerouslySetInnerHTML={html} onClick={this.handleClick}/>
+      );
+    }
     return (
-      <li data-notification-type={type} dangerouslySetInnerHTML={html} onClick={handleClick}/>
+      <li data-notification-type={type} onClick={this.handleClick}>{value}</li>
     );
   }
-  return (
-    <li data-notification-type={type} onClick={handleClick}>{value}</li>
-  );
-});
+}
 
 NotificationItem.propTypes = {
   type: PropTypes.string.isRequired,
