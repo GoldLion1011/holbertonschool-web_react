98fba5463d95c9db320c1b326c9c70056828a9c6
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-09-20 09:58:48 -0500

    initial push and README

---
+++ react_state/README.md
@@ -0,0 +1,86 @@
+# React State
+
+## Project Objectives
+
+This Holberton project is aimed at helping you understand and gain practical experience in several key aspects of full-stack web development using React. By the end of this project, you should be proficient in the following topics:
+
+### 1. State Management in React Components
+
+Understanding the state of a component or a container is fundamental to React development. You will learn how to:
+
+- Define and manage component state.
+- Update and modify the state using `setState` method.
+- Understand the importance of immutability in state updates.
+
+### 2. Component Lifecycle
+
+React components have a lifecycle that determines when they are created, updated, and destroyed. You will explore:
+
+- The various lifecycle methods and their purposes.
+- How to use lifecycle methods to perform tasks at specific points in a component's lifecycle.
+- Understanding component mounting, updating, and unmounting phases.
+
+### 3. Modifying State and Executing Code in the Right Order
+
+It's essential to know how to modify state and execute code in the correct sequence to avoid unexpected behavior. You will learn:
+
+- How to handle asynchronous operations.
+- Execute code in the appropriate lifecycle methods.
+- Use callbacks to ensure code executes in the right order.
+
+### 4. Controlled Components
+
+Controlled components are a vital concept in React, especially when working with forms. You will learn:
+
+- What controlled components are and why they are important.
+- How to manage form elements as controlled components.
+- The benefits of keeping form input values in the component's state.
+
+### 5. Working with Forms in React
+
+Forms are a common part of web applications. You will explore:
+
+- How to create forms in React.
+- Handling form submissions and user input.
+- Validating and processing form data.
+
+### 6. Reusable Components and State Lifting
+
+To maintain clean and maintainable code, you will discover how to:
+
+- Create smaller, reusable components.
+- Keep components pure by avoiding side effects and shared state.
+- Lift state to principal containers when needed to manage shared data.
+
+### 7. React Hooks
+
+React Hooks provide a way to use state and other React features in functional components. You will:
+
+- Learn how to create custom React Hooks.
+- Understand and use built-in hooks like `useState` and `useEffect`.
+- See how hooks can simplify state management and side effects in functional components.
+
+### 8. Testing State Changes with Enzyme
+
+Testing is a crucial aspect of software development. You will explore:
+
+- How to set up testing environments for React components.
+- Use Enzyme, a testing utility for React, to test state changes and component behavior.
+- Write unit tests to ensure your components work as expected.
+
+## Project Structure
+
+This project will provide practical exercises and examples for each of the objectives listed above. You will find code samples, hands-on exercises, and documentation to help you grasp these fundamental concepts of React development.
+
+## Getting Started
+
+To get started with this project, make sure you have the following prerequisites:
+
+- Basic knowledge of HTML, CSS, and JavaScript.
+- Node.js and npm (Node Package Manager) installed on your system.
+- A code editor of your choice (e.g., Visual Studio Code).
+
+
+## Conclusion
+
+By completing this project, you will have a solid understanding of React's core concepts and be well-prepared to develop dynamic and interactive web applications using React. Good luck with your learning journey!
---
+++ react_state/task_0/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,138 @@
+import React, { Component } from 'react';
+import { StyleSheet, css } from 'aphrodite';
+import closeIcon from '../assets/close-icon.png';
+import NotificationItem from './NotificationItem';
+import PropTypes from 'prop-types'
+import NotificationItemShape from './NotificationItemShape';
+
+class Notifications extends Component {
+  constructor(props) {
+    super(props);
+    this.markAsRead = this.markAsRead.bind(this);
+  };
+
+  shouldComponentUpdate(nextProps) {
+    return nextProps.listNotifications.length > this.props.listNotifications.length;
+  };
+
+  componentDidUpdate() {
+    console.log('Component has updated');
+  };
+
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+  };
+
+  render() {
+    const { displayDrawer, listNotifications } = this.props;
+
+    return (
+      <div id='container' className={css(styles.container)}>
+        <div className={`menuItem ${css(styles.menuItem)} ${css(styles.fadeBounceIn)}`}>
+          Your notifications
+        </div>
+        {displayDrawer && (
+          <div className={`notifications ${css(styles.notifications)}`}>
+            <img src={closeIcon} alt='close icon'
+              style={{ height: '15px', position: 'absolute', top: 10, right: 10 }}
+              aria-label='Close'
+              onClick={() => console.log('Close button has been clicked')}
+            ></img>
+
+            {listNotifications.length > 0 && <p>Here is the list of notifications</p>}
+            <ul className={css(styles.lists)}>
+              {listNotifications.length === 0 ? (
+                <p>No new notification for now</p>
+              ) : (
+                listNotifications.map(({ type, html, value, id }) => (
+                  <NotificationItem key={id} type={type} html={html} value={value} markAsRead={this.markAsRead} className={`notification-item ${css(type === 'default' ? styles.defaultNotifications : styles.urgentNotifications)}`} />
+                ))
+              )}
+            </ul>
+          </div>
+        )}
+      </div>
+    );
+  };
+};
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(NotificationItemShape),
+};
+
+Notifications.defaultProps = {
+  displayDrawer: false,
+  listNotifications: [],
+};
+
+const styles = StyleSheet.create({
+  container: {
+    float: 'right',
+    '@media screen and (max-width: 900px)': {
+      float: 'unset',
+    },
+  },
+  notifications: {
+    border: '2px dashed black',
+    padding: '1%',
+    float: 'right',
+    margin: '10px',
+    position: 'relative',
+    fontFamily: 'sans-serif',
+    '@media (max-width: 900px)': {
+      border: 'none',
+      padding: '0',
+      margin: '0',
+      float: 'none',
+      fontSize: '20px',
+      width: '100%',
+      height: '100%',
+      backgroundColor: '#fff',
+      position: 'absolute',
+      left: '0',
+      top: '0',
+    },
+  },
+  
+  defaultNotifications: {
+    color: 'blue',
+  },
+  urgentNotifications: {
+    color: 'red',
+  },
+  
+  menuItem: {
+    textAlign: 'right',
+    '@media (max-width: 900px)': {
+      display: 'none',
+    },
+  },
+
+  lists: {  
+    listStyleType: 'none',
+    padding: '0',
+    margin: '0',
+  },
+
+  fadeBounceIn: {
+    ':hover': {
+      animationName: [
+      {
+        '0%': { opacity: 0.5 }, 
+        '50%': { opacity: 0.75 },
+        '100%': { opacity: 1 },
+      },
+      {
+        '0%': { transform: 'translateY(0px)' },
+        '50%': { transform: 'translateY(-5px)' },
+        '100%': { transform: 'translateY(5px)' },
+      }
+    ],
+    animationDuration: ['1s', '0.5s'],
+    animationIterationCount: ['infinite', '3'],
+  }
+},
+});
+
+export default Notifications;
